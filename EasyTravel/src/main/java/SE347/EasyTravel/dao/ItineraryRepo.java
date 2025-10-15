5c9e3f97586e29fafc91e89fc03be47c9b1bcad2
bincasau <phinew331@gmail.com>
2025-10-15 11:52:17 +0700

    Sửa lỗi sai đối tượng file itineraryRepo

--- EasyTravel/src/main/java/SE347/EasyTravel/dao/ItineraryRepo.java
+++ EasyTravel/src/main/java/SE347/EasyTravel/dao/ItineraryRepo.java
@@ -1,9 +1,10 @@
 package SE347.EasyTravel.dao;
 
 import SE347.EasyTravel.entity.Image;
+import SE347.EasyTravel.entity.Itinerary;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource(path = "itinerary")
-public interface ItineraryRepo extends JpaRepository<Image, Integer> {
+public interface ItineraryRepo extends JpaRepository<Itinerary, Integer> {
 }
